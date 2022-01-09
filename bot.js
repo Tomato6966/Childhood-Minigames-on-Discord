09efad35b3e9ad77d529513721b37e7484e39a63
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2022-01-09 21:28:06 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -19,20 +19,21 @@ const { readdirSync } = require(`fs`);
     // define client variables
     client.allGames = new Collection();
     client.memoryGame = new Collection();
+    client.ladderGame = new Collection();
     client.commands = new Collection();
     client.allEmojis = require(`./src/json/emojis.js`);
     client.colors = require(`./src/json/colors.js`);
     client.allImages = require(`./src/json/images.js`);
 
 
-    client.db = require(`./src/json/database.js`);
+    client.db = require(`./src/util/database.js`);
     await client.db.init(); // initialize the database
 
     // Multiple Languages
-    client.la = {}
-    var langs = readdirSync(`./src/util/languages`)
-    for (const lang of langs.filter(file => file.endsWith(`.json`))) {
-        client.la[`${lang.split(`.json`).join(``)}`] = require(`./src/json/languages/${lang}`)
-    }
+    client.la = {};
+    readdirSync(`./src/json/languages`).filter(file => file.endsWith(`.json`)).forEach((lang => {
+        client.la[`${lang.replace(`.json`, ``)}`] = require(`./src/json/languages/${lang}`)
+    }));
+    Object.freeze(client.la);
     BotLoader.startBot(client, config);
 })();
